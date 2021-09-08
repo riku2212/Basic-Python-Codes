8e3e42e889279aa17167762b6ee0b6be886efdf0
riku2212 <90278420+riku2212@users.noreply.github.com>
2021-09-08 15:29:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+#Faulty Calculator
+
+c= input("Enter the operation you want to perform")
+
+a= int(input("Enter the first number"))
+
+b= int(input("Enter the second number"))
+
+if c=='+':
+
+ if a==56 and b==9:
+  print("77")
+
+ else:
+  print(a+b)
+
+if c=='*':
+ if a==45 and b==3:
+  print("555")
+
+ else:
+  print(a*b)
+
+if c=='/':
+ if a==56 and b==3:
+  print("6")
+
+ else:
+  print(a/b)
+
+
